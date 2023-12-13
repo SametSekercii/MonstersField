fbfb8384d93dba7b745fa993e88f1173dbcaefe2
Samet Şekerci <95055963+SametSekercii@users.noreply.github.com>
2023-12-13 19:00:18 +0300

    LoadScene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public GameData data;
+
+    void Awake()
+    {
+#if !UNITY_EDITOR
+        SaveManager.LoadData(data);
+#endif
+        SaveManager.LoadData(data);
+        SceneLoad();
+    }
+
+    void SceneLoad()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
