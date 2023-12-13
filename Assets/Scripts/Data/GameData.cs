f8c4eedab51057b5435c8c4e82924b69babfa8de
Samet Şekerci <95055963+WithRomance@users.noreply.github.com>
2023-12-13 18:20:24 +0300

    File Organization & 3rdParty

---
+++ Assets/Scripts/Data/GameData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObject/GameData", order = 1)]
+public class GameData : ScriptableObject
+{
+    [Header("Float & Int")]
+    public int gameLevel = 1;
+
+
+}
